f0fe73f391c8d5598871f18ec6613b91b65c600e
Egor <ItsAwware@yandex.ru>
2024-11-26 21:15:35 +0600

    build: new event listening

--- LumaRebalance/Plugin.cs
+++ LumaRebalance/Plugin.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx;
-using BepInEx.Logging;
 using LumaLibrary.Manager;
+using System.ArrayExtensions;
+using System.Linq;
 
 namespace LumaRebalance;
 
@@ -10,20 +11,42 @@ public class Plugin : BaseUnityPlugin
 {
     public const string ModGUID = "etc.shikaru.lumarebalance";
     public const string ModName = "LumaRebalance";
-    public const string Version = "0.0.1";
-    internal static new ManualLogSource Logger;
+    public const string Version = "0.0.2";
         
     private void Awake()
     {
-        // Plugin startup logic
-        Logger = base.Logger;
         Logger.LogInfo($"Plugin {ModGUID} is loaded!");
 
-        ShopManager.Instance.OnShopPlaced += OnShopPlaced;
+        ShopManager.Instance.OnShopBenchPlaced += OnShopPlaced;
+        ShopManager.Instance.OnShopInteract += OnShopInteract;
+
+        GameDataManager.Instance.OnGameInitialized += OnGameInitialized;
+    }
+
+    private void OnGameInitialized(bool val)
+    {
+        Logger.LogInfo("Game initialized!");
+        RebalanceBuyValue();
+        RebalanceSellValue();
+    }
+
+    private void RebalanceBuyValue()
+    {
+        ItemManager.Instance.UpdateBuyValue(LumaLibrary.Constant.GameItems.Gravel, 5);
+    }
+
+    private void RebalanceSellValue()
+    {
+        ItemManager.Instance.UpdateSellValue(LumaLibrary.Constant.GameItems.Gravel, 4);
     }
 
     private void OnShopPlaced(Shop instance)
     {
-        Logger.LogInfo($"Shop placed event: {instance.name}");
+
+    }
+
+    private void OnShopInteract(Shop instance) 
+    {
+        // ...
     }
 }
